0d892cbfa5aaff045ffd5e8d0a4bed50f12c62d8
arnieS06 <sharma.arnav2016@gmail.com>
2025-05-17 18:31:51 -0700

    Finished word size finder class

--- WordSizeAnalyzer.java
+++ WordSizeAnalyzer.java
@@ -1,3 +1,35 @@
-public class WordSizeAnalyzer extends TextAnalyzer {
+import java.util.Scanner;
 
+public class WordSizeAnalyzer extends TextAnalyzer {
+    public WordSizeAnalyzer() {
+        super();
+        reportPrefix = "Average word size: ";
+    }
+
+    public WordSizeAnalyzer(String t) {
+        super(t);
+        reportPrefix = "Average word size: ";
+    }
+
+    public void analyze() {
+        int totalCharCount = 0;
+        Scanner s = new Scanner(getText());
+
+        for (int i = 0; i < getText().length(); i++) {
+            if (!Character.isWhitespace(getText().charAt(i))) {
+                totalCharCount++;
+            }
+        }
+
+        // calculating word count
+        int wordCount = 0;
+
+        while (s.hasNext()) { 
+            s.next();
+            wordCount++;
+        }
+
+        results = totalCharCount/wordCount;
+
+    }
 }
